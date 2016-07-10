dd9eb6e86f6ec3a29752c93a8add5d76f90f0300
Will Lee <jaydi727@gmail.com>
2016-07-10 15:07:29 +0900

    no message

---
+++ app/src/main/java/edu/lecture/android/practice/SecondActivity.java
@@ -0,0 +1,24 @@
+package edu.lecture.android.practice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void hello(View view) {
+        TextView text_message = (TextView) findViewById(R.id.text_second_message);
+        text_message.setText(getResources().getString(R.string.hello));
+    }
+
+}
